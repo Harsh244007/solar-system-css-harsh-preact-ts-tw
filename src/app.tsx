134dab8d8d6f55db299ef14133346c727665776f
Harsh Patel <Patelharsh241999@gmail.com>
2023-09-18 14:13:13 +0530

    * Added Stars .

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,50 @@
+import { useEffect, useMemo } from "preact/hooks";
+
+interface StarProps {
+  size: number;
+  top: number;
+  left: number;
+}
+function Star({ size, top, left }: StarProps) {
+  const starStyle = {
+    width: `${size}px`,
+    height: `${size}px`,
+    top: `${top}%`,
+    left: `${left}%`,
+  };
+
+  return <div className="star" style={starStyle}></div>;
+}
 export function App() {
+  const generateStars = () => {
+    const newStars = [];
+    for (let i = 0; i < 200; i++) {
+      const size = Math.random() * 4 + 0.1;
+      const top = Math.random() * 100;
+      const left = Math.random() * 100;
+      newStars.push({ size, top, left });
+    }
+    return newStars;
+  };
+
+  const stars = useMemo(() => generateStars(), []);
+
+  useEffect(() => {
+    document.body.style.background =
+      window.location.hostname ==
+      "solar-system-css-harsh-preact-ts-tw.vercel.app"
+        ? "#1c1917"
+        : "transparent";
+  }, []);
   return (
     <>
+      <div className="stars">
+        {stars.map((star, index) => (
+          <Star key={index} {...star} />
+        ))}
+      </div>
       <div className="container relative w-40 h-40">
-        <div className="sun absolute top-15 left-13 w-10 h-10 rounded-full shadow-2xl bg-yellow-300">
-        </div>
+        <div className="sun absolute top-15 left-13 w-10 h-10 rounded-full shadow-2xl bg-yellow-300"></div>
         <div className="earth absolute top-5 left-3 w-30 h-30 border-solid border-white border-t-0.1 border-r-0.1 rounded-full animate-orbit">
           <div className="moon absolute top-0 left-21 w-9 h-9 border-solid border-white border-t-0.1 border-r-0.1 rounded-full animate-orbit"></div>
         </div>
@@ -13,6 +54,12 @@ export function App() {
             width: 40em;
             height: 40em;
             position: relative;
+          }
+          .star{
+            position:absolute;
+            background-color:white;
+            border-radius:50%;
+            z-index:-1;
           }
           .sun {
             position: absolute;
